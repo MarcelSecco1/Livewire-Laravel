636397c639608e7a7dff1ac8025387bae450f0c9
Marcel Secco <marcelsecco1@gmail.com>
2023-11-25 17:49:56 -0300

    First Commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
+    }
 }
