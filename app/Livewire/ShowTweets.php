cbfd8c0f9f9e2a9cd3bcd51f63a61f35e0ae0324
Marcel Secco <marcelsecco1@gmail.com>
2023-11-25 18:31:10 -0300

    update2.0

--- app/Livewire/ShowTweets.php
+++ app/Livewire/ShowTweets.php
@@ -9,13 +9,20 @@ class ShowTweets extends Component
 {
     public $content = 'Apenas um teste';
 
+    //validações
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
+
     public function render()
     {
+        //retorna todos os tweets e possui o relacionamento com o user
         $tweets = Tweet::with('user')->get();
 
         return view(
             'livewire.show-tweets',
             [
+                //passa os tweets para a view
                 'tweets' => $tweets
             ]
         );
@@ -25,11 +32,16 @@ class ShowTweets extends Component
 
     public function create()
     {
+        //chama as validações
+        $this->validate();
+
+        //metodo de criação de tweet
         Tweet::create([
             'content' => $this->content,
             'user_id' => 1
         ]);
 
+        //limpa o campo de tweet
         $this->content = '';
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -5,6 +5,11 @@
 
     <form method="post" wire:submit.prevent="create">
         <input type="text" name="content" id="content" wire:model="content">
+        {{-- se existe um erro ele exibe a messagem do erro--}}
+        @error('content')
+            {{$message}}
+        @enderror
+
         <button type="submit">Criar Tweet</button>
     </form>
 
